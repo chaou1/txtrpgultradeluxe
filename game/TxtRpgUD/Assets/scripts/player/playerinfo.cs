3fdfb52e97f4d16512f5fd9b6fdec444e8c32d41
Ruihaolol <ruihao111@gmail.com>
2022-08-30 19:54:17 +0200

    dfggg

--- game/TxtRpgUD/Assets/scripts/player/playerinfo.cs
+++ game/TxtRpgUD/Assets/scripts/player/playerinfo.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class playerinfo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string Name = "Pain";
+    public double maxhealth;
+    public double health;
+    public double attack;
+    public double defense;
+    public double maxmana;
+    public double mana;
+    public double geld;
+    public double neu;
+    public double exp;
+    public double level;
     void Start()
     {
+        maxhealth = 10;
+        health = 10;
+        attack = 1;
+        defense = 1;
+        maxmana = 10;
+        mana = 10;
+        geld = 100;
+        exp = 0;
+        level = 1;
+    }
+    public void upgrade(string stat, int p)
+    {
+        switch (stat) {
+            case maxhealth:
+                break;
         
+        
+        }
     }
 
-    // Update is called once per frame
+    public void Crit()
+    {
+
+
+
+
+    }
     void Update()
     {
-        
+
     }
 }
